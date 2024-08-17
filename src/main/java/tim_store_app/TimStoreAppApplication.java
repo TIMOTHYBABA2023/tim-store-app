025049886a582fbc11bdc5ec9934356f845f337f
Timothy <timothybaba01@gmail.com>
2024-08-17 23:55:15 +0100

    Initial commit of Spring application

---
+++ src/main/java/tim_store_app/TimStoreAppApplication.java
@@ -0,0 +1,13 @@
+package tim_store_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimStoreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimStoreAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tim_store_app/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package tim_store_app.repository;
+
+//import org.springframework.data.jpa.repository.JpaRepository;
+//public interface UserRepository extends JpaRepository<Long, User> {
+//}
